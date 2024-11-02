b4ec694a82aa673061f86c10d0ee0ae68622cbf4
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-11-02 11:08:07 +0530

    Update llama.py

--- chatgpt/llama.py
+++ chatgpt/llama.py
@@ -5,13 +5,12 @@ from .paste import create_paste
 @Client.on_message(filters.command("llama"))
 async def llamachat(client, message):
     if len(message.command) < 2:
-        await message.reply("Please provide the query")
-        return 
+        return await message.reply_text("Please provide the query")
         
     query = " ".join(message.command[1:])
     ai = api.llama(query)
     if len(ai) > 3700:
-        result = await create_paste(paste_content)
+        result = await create_paste(ai)  
         await message.reply_text(result["url"])
     else:
         await message.reply_text(ai)
