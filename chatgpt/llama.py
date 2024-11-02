ec01a8b269b50039f546be5820c377ad02cd231b
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-11-02 09:44:41 +0530

    Update llama.py

--- chatgpt/llama.py
+++ chatgpt/llama.py
@@ -1,11 +1,16 @@
 from pyrogram import Client, filters
 from HorridAPI import api
+from .paste import create_paste
 
 @Client.on_message(filters.command("llama"))
 async def llama(client, message):
     if len(message.command) < 2:
         await message.reply("Please provide the query")
-    else:
-        query = " ".join(message.command[1:])
-        ai = api.llama(query)
-        await message.reply_text(ai)
+        return 
+        
+    query = " ".join(message.command[1:])
+    ai = api.llama(query)
+    if len(ai) > 3700:
+        result = await create_paste(paste_content)
+        await message.reply_text(result["url"])
+    await message.reply_text(ai)
