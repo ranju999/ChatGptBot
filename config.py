f228e41087185175cd17a460913753b7f2e76c2f
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-11-02 11:42:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from os import environ
+import re
+
+id_pattern = re.compile(r'^.\d+$')
 
 # bot
 API_ID = int(environ.get('API_ID', ''))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
+ADMINS = [int(admins) if id_pattern.search(admins) else admins for admins in environ.get('ADMINS', '1867106198').split()]
 
 # database
 
