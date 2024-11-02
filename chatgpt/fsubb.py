96c9e3271d0b2cc353a6e0f4ba44114bf7ff9054
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-11-02 12:18:50 +0530

    Create fsubb.py

---
+++ chatgpt/fsubb.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters, enums
+from config import FSUB_ID
+
+async def not_subscribed(client, message):    
+    try:
+        user = await client.get_chat_member(FSUB_ID, message.from_user.id)
+    except Exception:
+        pass
+    else:
+        if user.status != enums.ChatMemberStatus.BANNED:
+            return False
+    return True
