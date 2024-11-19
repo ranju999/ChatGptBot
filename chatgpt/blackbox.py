9bf1609fb9eba0850d1aaa09138509e31b3afc97
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-11-19 17:47:11 +0530

    Added reply text support

--- chatgpt/blackbox.py
+++ chatgpt/blackbox.py
@@ -1,23 +1,35 @@
-from HorridAPI import Mango
-from pyrogram import filters, Client 
 from .paste import create_paste
+from pyrogram.types import *
+from config import FSUB_ID
+from .fsubb import not_subscribed
+from pyrogram import Client, filters
+from HorridAPI import Mango
 
 @Client.on_message(filters.command("blackbox"))
-async def blackbox(client, message):   
+async def blackbox(client, message):
+    if FSUB_ID and not await not_subscribed(client, message):
+        invite_link = await client.create_chat_invite_link(int(FSUB_ID))
+        buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ", url=invite_link.invite_link)]]
+        text = f"Hey {message.from_user.mention}\n You are not joined this channel"
+        await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+        return 
     if len(message.command) < 2:
-        await message.reply_text("Give An Input!")
-        return
-    
-    query = " ".join(message.command[1:])
-    k = await message.reply_text("🔍")
-    mango = Mango()  
+        return await message.reply_text("Please provide the query")
+            
+    reply = message.reply_to_message    
+    if reply:
+        query = f"Old conversation: {l.text}\n\nNew conversation: {message.text}"
+    else:
+        query = " ".join(message.command[1:])
+    msg = await message.reply_text("🔍")
+    mango = Mango()
     response = mango.chat.completions.create(
-        model="blackbox",
+        model="blackbox",      
         messages=[{"role": "user", "content": query}]
     )
-
     if len(response.text) > 3700:        
         result = await create_paste(response.text)  
-        await k.edit(result)       
+        await msg.edit(result)       
     else:
-        await k.edit(response.text)   
+        await msg.edit(response.text)
+      
