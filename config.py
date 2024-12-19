7f121ea388c6a5fb61ebe269246fd077ddf9efd3
ranju999 <ranjuvishwakarma.50@gmail.com>
2024-12-19 10:36:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ id_pattern = re.compile(r'^.\d+$')
 # bot
 API_ID = int(environ.get('API_ID', '12380656'))
 API_HASH = environ.get('API_HASH', 'd927c13beaaf5110f25c505b7c071273')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-ADMINS = [int(admins) if id_pattern.search(admins) else admins for admins in environ.get('ADMINS', '1867106198').split()]
+BOT_TOKEN = environ.get('BOT_TOKEN', '7232006758:AAGV-HPIfJi3k-gz2_YR6_-1k7706NEPD-I')
+ADMINS = [int(admins) if id_pattern.search(admins) else admins for admins in environ.get('ADMINS', '5977931010').split()]
 
 # bs
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2114619001'))
